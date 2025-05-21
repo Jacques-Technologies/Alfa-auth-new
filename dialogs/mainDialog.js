57fc2ba1a72391e72792d8fe90dbc563f3d25a4b
Artemio <artemio.ert@gmail.com>
2025-05-21 04:42:11 -0600

    fix

--- dialogs/mainDialog.js
+++ dialogs/mainDialog.js
@@ -3,7 +3,6 @@
 
 const { ConfirmPrompt, DialogSet, DialogTurnStatus, OAuthPrompt, WaterfallDialog } = require('botbuilder-dialogs');
 const { LogoutDialog } = require('./logoutDialog');
-const { SimpleGraphClient } = require('../simpleGraphClient');
 const { CardFactory } = require('botbuilder-core');
 
 const CONFIRM_PROMPT = 'ConfirmPrompt';
@@ -73,7 +72,6 @@ class MainDialog extends LogoutDialog {
             await stepContext.context.sendActivity('Login was not successful, please try again.');
             return await stepContext.endDialog();
         } else {
-            const client = new SimpleGraphClient(tokenResponse.token);
             const me = await client.getMe();
             const title = me ? me.jobTitle : 'Unknown';
             await stepContext.context.sendActivity(`You're logged in as ${me.displayName} (${me.userPrincipalName}); your job title is: ${title}; your photo is: `);
